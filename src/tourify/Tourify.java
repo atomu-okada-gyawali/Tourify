273715a497c39aec38cfe62753eb4d7c35c1932d
b-shhhh <159143153+b-shhhh@users.noreply.github.com>
2024-05-31 21:40:36 +0545

    Project start with registration page

---
+++ src/tourify/Tourify.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tourify;
+
+/**
+ *
+ * @author Acer
+ */
+public class Tourify {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame nf = new NewJFrame();
+        nf.setVisible(true);
+    }
+    
+}
