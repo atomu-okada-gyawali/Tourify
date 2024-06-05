a98711fa975e3f354c5fb031024d52b25ba356fb
sailesh-tamang <saileshtamang909@gmail.com>
2024-06-05 21:51:39 +0545

    Booking dashboard

---
+++ src/tourify/Tourify.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tourify;
+
+import tourify.View.MainApp;
+
+/**
+ *
+ * @author lenevo
+ */
+public class Tourify {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainApp mApp = new MainApp();
+        mApp.setVisible(true);
+    }
+    
+}
